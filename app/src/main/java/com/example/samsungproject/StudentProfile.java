f6474f1601991a2701d728b87b2d2e1543b59a20
Mishka <C.o.o.l.p.e.p.p.e.r@yandex.ru>
2023-05-23 19:50:00 +0300

    sds

--- app/src/main/java/com/example/samsungproject/StudentProfile.java
+++ app/src/main/java/com/example/samsungproject/StudentProfile.java
@@ -1,18 +1,33 @@
 package com.example.samsungproject;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class StudentProfile extends AppCompatActivity {
 
+    private Button changeStudentAvatar;
+    private ImageView studentAvatar;
+    private int SELECT_PICTURE = 200;
     String[] activityMenu = {"Профиль", "Портфолио", "Результаты ПА", "Выход"};
 
     @Override
@@ -20,9 +35,11 @@ public class StudentProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
 
+        ImageView studentAvatar = findViewById(R.id.user_avatar);
         EditText password = findViewById(R.id.current_password);
         EditText newPassword = findViewById(R.id.new_password);
         EditText repeatPassword = findViewById(R.id.repeat_password);
+        Button changeStudentAvatar = findViewById(R.id.change_avatar_btn);
         Button changePasswordButton = findViewById(R.id.change_password_btn);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -59,4 +76,38 @@ public class StudentProfile extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+    
+    public void pickUserAvatar(View view) {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        launchSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap;
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                            studentAvatar.setImageBitmap(selectedImageBitmap);
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
 }
