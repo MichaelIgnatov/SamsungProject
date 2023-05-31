7695312c43520026204c03539115cd35bfc75b6b
Mishka <C.o.o.l.p.e.p.p.e.r@yandex.ru>
2023-05-31 19:17:04 +0300

    api

--- app/src/main/java/com/example/samsungproject/Student.java
+++ app/src/main/java/com/example/samsungproject/Student.java
@@ -1,4 +1,35 @@
 package com.example.samsungproject;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public class Student {
+    public static final String serverURl = "http://192.168.0.11:80";
+    public Retrofit retrofit;
+    public StudentService studentService;
+
+    public static class StudentData {
+        public int id;
+        public String name;
+        public String login;
+        public String password;
+        public String birth_date;
+        public String create_date;
+        public int teacher_id;
+        public byte avatar;
+    }
+    public interface StudentService {
+        @GET("/api/get_student/{login}/{password}")
+        Call<StudentData> studentLogin(@Path("login") String login, @Path("password") String password);
+    }
+
+    public void Student() {
+        retrofit = new Retrofit.Builder().baseUrl(serverURl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        studentService = retrofit.create(StudentService.class);
+    }
 }
--- app/src/main/java/com/example/samsungproject/StudentLoginActivity.java
+++ app/src/main/java/com/example/samsungproject/StudentLoginActivity.java
@@ -3,22 +3,30 @@ package com.example.samsungproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class StudentLoginActivity extends AppCompatActivity {
+    Student studentObj;
+    String login;
+    String password;
+    boolean flag;
+    EditText studentPasswordTextedit;
+    EditText studentLoginTextedit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
-
-        EditText studentLoginTextedit = findViewById(R.id.student_login);
-        EditText studentPasswordTextedit = findViewById(R.id.student_password);
-        Button studentLoginButton = findViewById(R.id.login);
-        Button backButton = findViewById(R.id.back);
+        studentLoginTextedit = findViewById(R.id.student_login);
+        studentPasswordTextedit = findViewById(R.id.student_password);
+        studentObj = new Student();
     }
 
     public void backToActivity(View view) {
@@ -26,8 +34,32 @@ public class StudentLoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void studentLogin(View view) {
-        Intent intent = new Intent(this, StudentProfile.class);
-        startActivity(intent);
+    public void studentLoginClick(View view) {
+        //login = studentLoginTextedit.getText().toString();
+        //password = studentPasswordTextedit.getText().toString();
+        flag = false;
+        Call<Student.StudentData> call = studentObj.studentService.studentLogin("Qwe", "123");
+        call.enqueue(new Callback<Student.StudentData>() {
+            @Override
+            public void onResponse(Call<Student.StudentData> call, Response<Student.StudentData> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Toast.makeText(getApplicationContext(), "Вход выполнен", Toast.LENGTH_SHORT).show();
+                    flag = true;
+                } else{
+                    Toast.makeText(getApplicationContext(), "Что-то пошло не так", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Student.StudentData> call, Throwable t) {
+
+            }
+        });
+
+        if(flag == true) {
+            Intent intent = new Intent(this, StudentProfile.class);
+            startActivity(intent);
+        }
     }
 }
